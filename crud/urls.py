657b3e9bca3cf54ba738d8acf0b1e46837167609
bhj8286 <bjs219@naver.com>
2023-11-01 14:41:56 +0900

    Update기능 구현

--- crud/urls.py
+++ crud/urls.py
@@ -25,11 +25,16 @@ urlpatterns = [
     path('index/', views.index),
     path('posts/<int:id>/', views.detail),
 
-    #Create
+    # Create
     path('posts/new/', views.new),
     path('posts/create/', views.create),
 
 
-    #Delete
+    # Delete
     path('posts/<int:id>/delete/', views.delete),
+
+    # Update
+    path('posts/<int:id>/edit/', views.edit),
+    path('posts/<int:id>/update/', views.update),
+
 ]
